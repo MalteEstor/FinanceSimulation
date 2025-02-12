68466295b87d9bbea0ccd919d26e206a511dc56c
Malte Estor <malte.estor@diconium.com>
2025-02-12 14:02:49 +0100

    init

---
+++ src/main/java/com/IHKProject/finance_simulation/FinanceSimulationApplication.java
@@ -0,0 +1,13 @@
+package com.IHKProject.finance_simulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceSimulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceSimulationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/IHKProject/finance_simulation/FinanceSimulationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IHKProject.finance_simulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceSimulationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
